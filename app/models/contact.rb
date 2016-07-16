3b13c6380b2b96831b7f7202ef0f22e2e6593da6
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-07-16 11:39:42 -0700

    rails g model PhoneNumber contact_id:integer name phone_number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  has_many :phone_numbers
+  validates :name_first, presence: true
+  validates :name_last, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716181842) do
+ActiveRecord::Schema.define(version: 20160716183036) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name_first"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20160716181842) do
     t.string   "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
+    t.integer  "contact_id"
+    t.string   "name"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
